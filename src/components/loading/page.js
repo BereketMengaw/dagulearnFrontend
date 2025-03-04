e09d85284e1e4acf8b910c072a316aa90818422a
BereketMengaw <165939233+BereketMengaw@users.noreply.github.com>
2025-03-04 03:51:35 +0300

    Update page.js - bg is removed

--- src/components/loading/page.js
+++ src/components/loading/page.js
@@ -2,7 +2,7 @@ import img from "../../../public/favicon.png";
 
 export default function LoadingSpinner() {
   return (
-    <div className="relative flex justify-center items-center w-screen h-screen gap-5 dark:bg-gray-900">
+    <div className="relative flex justify-center items-center w-screen h-screen gap-5 ">
    <div class='flex space-x-2 justify-center items-center bg-white h-screen dark:invert'>
 	<span class='sr-only'>Dagulearn...</span>
 	<div class='h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.3s]'></div>
